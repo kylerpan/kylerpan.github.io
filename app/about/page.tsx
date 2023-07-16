f48639546a6e83a383e6023b2c4b498a32f7504e
kylerpan <kylerpan691@gmail.com>
2023-07-16 10:46:48 -0700

    added new routes for nav bar

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import Intro from "../../components/Intro";
+
+export default function About() {
+  return (
+    <main className="flex flex-grow flex-shrink h-full w-full flex-col items-center justify-center p-10">
+      <div className="flex flex-col z-10 h-full w-full items-center justify-center font-sans lg:flex gap-3">
+        <Intro
+          description="Lets take a look at"
+          header="My Background"
+          button="Learn more"
+        />
+      </div>
+    </main>
+  );
+}
