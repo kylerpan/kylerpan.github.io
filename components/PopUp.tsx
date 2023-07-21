c855ca65e802cd0499914a391659f02ea4f4d67b
kylerpan <kylerpan691@gmail.com>
2023-07-21 15:02:32 -0700

    Added timeline for multiple positions in pop up

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import Link from "next/link";
 
 import Card from "./Card";
 import work from "@/public/assets/work.json";
 import projects from "@/public/assets/projects.json";
+
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 
 interface PopUpProps {
   index: number;
@@ -33,13 +43,47 @@ const PopUp = (props: PopUpProps) => {
             &#10005;
           </Link>
         </div>
-        <p className="text-3xl">{data[props.index].position}</p>
-        <p>
-          {data[props.index].location} {data[props.index].date}
-        </p>
-        <p className="text-xl">{data[props.index].description}</p>
-        <p className="text-xl font-bold">Skills Used</p>
-        <p>{data[props.index].skills}</p>
+        {data[props.index].date.length == 1 ? (
+          <div className="flex flex-col gap-5">
+            <p className="text-3xl">{data[props.index].position}</p>
+            <p>
+              {data[props.index].date} &#x2022; {data[props.index].location}
+            </p>
+            <p className="text-xl">{data[props.index].description}</p>
+            <p className="text-xl font-bold">Skills Used</p>
+            <p>{data[props.index].skills}</p>
+          </div>
+        ) : (
+          <Timeline className="flex justify-self-start p-0 gap-5">
+            {Array.from({ length: data[props.index].date.length }).map(
+              (el: any, i: number) => (
+                <TimelineItem>
+                  <TimelineOppositeContent className="flex-none p-0" />
+                  <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <div className="flex flex-col gap-5">
+                      <p className="text-3xl">
+                        {data[props.index].position[i]}
+                      </p>
+                      <p>
+                        {data[props.index].date[i]} &#x2022;{" "}
+                        {data[props.index].location}
+                      </p>
+                      <p className="text-xl">
+                        {data[props.index].description[i]}
+                      </p>
+                      <p className="text-xl font-bold">Skills Used</p>
+                      <p>{data[props.index].skills[i]}</p>
+                    </div>
+                  </TimelineContent>
+                </TimelineItem>
+              )
+            )}
+          </Timeline>
+        )}
       </div>
       <div className="flex flex-col items-center">
         <h2 className="text-4xl font-bold">Other Work</h2>
